82f87e5e83d240e3be5608b7e5633f481b15b9cf
idqam <owenjave5@gmail.com>
2025-02-09 14:19:22 -0500

    trying phone

--- src/components/sections/HomeSec.tsx
+++ src/components/sections/HomeSec.tsx
@@ -5,10 +5,10 @@ export const HomeSec = () => {
       className="min-h-screen bg-black flex  items-center justify-center relative"
     >
       <div className="text-center z-10 px-4">
-        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-300 to-red-300 via-purple-300 bg-clip-text text-transparent leading-right">
+        <h1 className="text-4xl sm:text-xl md:text-2xl lg:text-5xl font-bold mb-6 bg-gradient-to-r from-blue-300 to-red-300 via-purple-300 bg-clip-text text-transparent leading-right">
           Hi! Im Owen
         </h1>
-        <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+        <p className="text-gray-400 text-lg sm:text-sm mb-8 max-w-lg mx-auto">
           I am a full-stack developer with a passion for building robust backend
           systems, intuitive frontend interfaces, and overall clean & scalable
           web apps. I make it a priority to continuously learn new technologies
